463590a9dea591c10ea7ae2943b557e533f98bf7
Joshua <liujoshuath14@gmail.com>
2018-01-06 19:16:34 -0500

    updated to read by byte size

--- split_file.py
+++ split_file.py
@@ -4,30 +4,36 @@ Created on Fri Jan  5 15:19:09 2018
 
 @author: Joshua
 """
-# splits file of rows into multiple files
-# file name or directory, extension, and max line count
-def split_file(filename, ext, max_count=10**3):
-    file_cnt = 0
-    idx = 1
-    
-    # built in python function returns a file object
-    try:
-        with open(filename + '.' + ext, mode='r') as myfile:
-            write_file = open(filename + '_split_' + str(file_cnt) + '.csv', mode='w')
-            
-            for row in myfile:
-                
-                if (idx % max_count) == 0:
-                    file_cnt += 1
-                    write_file.close()
-                    write_file = open(filename + '_split_' + str(file_cnt) + '.' + ext, mode='w')
-                
-                write_file.write(row)
-                idx += 1
-        
-        myfile.close()
-        write_file.close()
-    except IOError:
-        print('Could not find file:', filename + '.' + ext)
 
-split_file('train', 'csv', 10 ** 7)
+# splits file of rows into multiple files
+# file name or directory, extension, and file size in bytes
+def split_file(filename, ext, size=2 ** 28, newline='\n'):
+    new_filename = filename + '_split_'
+    file_cnt = 0
+    tmp = ''
+    
+    # open is a built-in python function that returns a file object
+    with open(filename + '.' + ext, mode='r') as myfile:        
+        
+        data = myfile.read(size)
+        while (data != ''):
+            
+            idx = find_newline(data, newline)
+            
+            with open(new_filename + str(file_cnt) + '.' + ext, mode='w') as new_file:
+                new_file.write(tmp + data[:idx+1])
+
+            tmp = data[idx+1:]
+            data = myfile.read(size)
+            file_cnt += 1
+
+# returns the index of the last new line character
+def find_newline(text, newline='\n'):
+    index = len(text)
+    found = False
+    while(not found and index > -1):
+        index -= 1
+        found = text[index] == newline
+    return index
+
+split_file('train', 'csv', size=2 ** 28)
